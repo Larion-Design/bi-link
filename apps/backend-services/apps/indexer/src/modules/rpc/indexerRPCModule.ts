6bbca8241c152c894b21fe0e7279d79efeeed07e
Alex Tudor <18462175+alextudor1992@users.noreply.github.com>
2023-08-21 10:55:40 +0100

    fixed runtime errors

--- apps/backend-services/apps/indexer/src/modules/rpc/indexerRPCModule.ts
+++ apps/backend-services/apps/indexer/src/modules/rpc/indexerRPCModule.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common'
 import { IndexerModule } from '../indexer/indexerModule'
 import { MappingModule } from '../mapping/mappingModule'
+import { IndexerSchedulerModule } from '../scheduler/indexerSchedulerModule'
 import { SearchModule } from '../search/searchModule'
 import { IndexerController } from './controllers/indexerController'
 import { MappingController } from './controllers/mappingController'
 import { SearchController } from './controllers/searchController'
 
 @Module({
-  imports: [MappingModule, IndexerModule, SearchModule],
+  imports: [IndexerSchedulerModule, MappingModule, IndexerModule, SearchModule],
   controllers: [IndexerController, MappingController, SearchController],
 })
 export class IndexerRPCModule {}
