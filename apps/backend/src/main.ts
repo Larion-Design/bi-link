04a68843218bf50e62454e0f6a64fcd2c629726c
Alex Tudor <18462175+alextudor1992@users.noreply.github.com>
2024-01-09 15:13:10 +0000

    updated healthcheck for minio

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,13 +5,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule)
-    app.enableCors({ origin: true })
-    app.enableShutdownHooks()
-    await app.listen(44023)
-  } catch (e) {
-    console.error(e)
-  }
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({ origin: true })
+  app.enableShutdownHooks()
+  await app.listen(44023)
 }
 void bootstrap()
