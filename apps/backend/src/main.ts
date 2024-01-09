a120f518003af0794119d662bb61bab6c5acdffd
Alex Tudor <18462175+alextudor1992@users.noreply.github.com>
2024-01-09 13:31:00 +0000

    temp error logging

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,15 +1,17 @@
 import 'tslib'
 import 'tsconfig-paths/register'
 
-import * as compression from 'compression'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.use(compression())
-  app.enableCors({ origin: true })
-  app.enableShutdownHooks()
-  await app.listen(44023)
+  try {
+    const app = await NestFactory.create(AppModule)
+    app.enableCors({ origin: true })
+    app.enableShutdownHooks()
+    await app.listen(44023)
+  } catch (e) {
+    console.error(e)
+  }
 }
 void bootstrap()
