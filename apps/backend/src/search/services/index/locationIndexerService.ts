c9baf5c61e13788df5290fef1974b0840fe3cf59
Alex Tudor <18462175+alextudor1992@users.noreply.github.com>
2024-02-21 12:01:14 +0000

    Revert "fixed locations indexing error"
    This reverts commit 62ebc0c3827a79149948051d27e60b0ef17b3811.

--- apps/backend/src/search/services/index/locationIndexerService.ts
+++ apps/backend/src/search/services/index/locationIndexerService.ts
@@ -8,15 +8,25 @@ export class LocationIndexerService {
     street,
     number,
     door,
+    coordinates: { lat = 0, long = 0 },
     otherInfo,
     county,
     locality,
     zipCode,
     building,
     country,
-  }: Location) =>
-    [street, number, door, otherInfo, county, locality, zipCode, building, country].join(' ')
+  }: Location): LocationIndex => ({
+    street,
+    number,
+    door,
+    otherInfo,
+    county,
+    locality,
+    zipCode,
+    building,
+    country,
+    coordinates: [Number(lat), Number(long)],
+  })
 
-  createLocationsIndexData = (locations: Location[]) =>
-    locations?.map((location) => this.createLocationIndexData(location) ?? '')
+  createLocationsIndexData = (locations: Location[]) => locations?.map(this.createLocationIndexData)
 }
